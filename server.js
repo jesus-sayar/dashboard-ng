3f3c8959b1df2879056f698bf43534ef37b70d2f
Jesús Sayar <sayar.jesus@gmail.com>
2022-01-29 22:28:34 +0100

    fix heroku

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const express = require('express');
 const app = express();
 app.use(requireHTTPS);
 
-app.use(express.static('./dist/dashboard'));
+app.use(express.static('build'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/dashboard/'});
+    res.sendFile('index.html', {root: 'build'});
 });
 
 app.listen(process.env.PORT || 8080);
